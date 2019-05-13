624f85730e39e81e03d82589c6c8b2122155cdf3
Phil Greco <greco.41@osu.edu>
2019-05-13 18:23:19 -0400

    Barrel Elevation

--- 02_BattleTank/BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ 02_BattleTank/BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -49,7 +49,7 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection) {
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 
-	Barrel->Elevate(5);
+	Barrel->Elevate(DeltaRotator.Pitch);
 }
 
 
